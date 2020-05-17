d651abf7f0fc9b74927045e3330da95916a12f45
ThilakaHome <v.thilaka@gmail.com>
2020-05-17 16:37:40 +1000

    bulkhead

--- movie-catalog-service/src/main/java/com/thilaka/moviecatalogservice/services/MovieInfo.java
+++ movie-catalog-service/src/main/java/com/thilaka/moviecatalogservice/services/MovieInfo.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.thilaka.moviecatalogservice.models.CatalogItem;
 import com.thilaka.moviecatalogservice.models.Movie;
 import com.thilaka.moviecatalogservice.models.Rating;
@@ -15,7 +16,14 @@ public class MovieInfo {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+			threadPoolKey = "movieInfoPool",
+			threadPoolProperties = {
+			         @HystrixProperty(name = "coreSize", value = "20"),
+			         @HystrixProperty(name = "maxQueueSize", value = "10"),
+
+			    }
+			)
 	public CatalogItem getCatalogItem(Rating rating) {
 		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 		return new CatalogItem(movie.getName(),"Desc",rating.getRating());
