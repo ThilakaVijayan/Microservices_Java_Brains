bee1fdfe10718081b6e471a4d465b6ca34c962b5
ThilakaHome <v.thilaka@gmail.com>
2020-05-17 16:28:54 +1000

    Configure Hysterix behaviour

--- movie-catalog-service/src/main/java/com/thilaka/moviecatalogservice/services/UserRatingInfo.java
+++ movie-catalog-service/src/main/java/com/thilaka/moviecatalogservice/services/UserRatingInfo.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.thilaka.moviecatalogservice.models.Rating;
 import com.thilaka.moviecatalogservice.models.UserRating;
 
@@ -16,7 +17,15 @@ public class UserRatingInfo {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@HystrixCommand(fallbackMethod = "getFallbackUserRating")
+	
+	
+	@HystrixCommand(fallbackMethod = "getFallbackUserRating",
+			commandProperties = {
+					@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+					@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+			        @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+			        @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000")
+			})
 	public UserRating getUserRating(String userId) {
 		return restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId, UserRating.class);
 	}
